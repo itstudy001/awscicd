92c7b5fb79f6ec0797eb8945e16f218f38c6fb8e
itstudy <itstudy@kakao.com>
2024-11-18 14:02:05 +0900

    image build

---
+++ src/main/java/com/adamsoft/awscicd/FrontController.java
@@ -0,0 +1,12 @@
+package com.adamsoft.awscicd;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FrontController {
+    @RequestMapping("/")
+    public String index() {
+        return "안녕하세요";
+    }
+}
